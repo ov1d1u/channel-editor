99c4852eda9ecfdc7638df12aa81da572db9304e
Ovidiu D. Nitan <nitanovidiu@gmail.com>
2018-07-31 21:06:46 +0300

    Fix imgur upload

--- imgur.py
+++ imgur.py
@@ -1,26 +1,25 @@
-import pycurl, cStringIO, base64
+import pycurl, cStringIO, base64, json
 
 class Imgur:
 	def __init__(self):
-		self.key = '72e52d4bdd04c9dd7e057eda9ef80cbf'
+		self.key = 'ee511d09eedef3d'
 		
 	def upload(self, data, callback=None, *args):
 		response = cStringIO.StringIO()
 		c = pycurl.Curl()
 		data = base64.encodestring(data)
 		values = [
-			  ("key", self.key),
-			  ("image", data)]
+			  ("image", data)
+		]
 
-		c.setopt(c.URL, "http://imgur.com/api/upload.xml")
+		c.setopt(c.URL, "https://api.imgur.com/3/image")
 		c.setopt(c.WRITEFUNCTION, response.write)
+		c.setopt(c.HTTPHEADER, ["Authorization: Client-ID {0}".format(self.key)])
 		c.setopt(c.HTTPPOST, values)
 
 		c.perform()
 		c.close()
-		r = response.getvalue().split('<original_image>')
-		rr = r[1].split('</original_image>')
-		reply = rr[0]
+		r_dict = json.loads(response.getvalue())
 		if callback:
 			callback(reply, args)
-		return reply
+		return r_dict["link"]
